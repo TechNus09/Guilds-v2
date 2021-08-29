a28e4ae4bdf5c092d2af8ab3d0a28d97b67ac18e
TechNus09 <59443272+TechNus09@users.noreply.github.com>
2021-08-29 22:01:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -106,6 +106,38 @@ guilds_counter_int  = {'IMMORTAL': 0, 'OWO': 0, 'EXP': 0, 'BRX': 0, 'RNG': 0, 'L
                         'RICK': 0, 'ULTR': 0, 'KAMI': 0, 'QUAN': 0, 'FORST': 0, 'DWIKI': 0, 'LBCL': 0, 'EML': 0, 'HUONG': 0, 'AFK': 0, 'NO': 0,
                         'DUCKS': 0, 'XERRA': 0, 'THAT': 0, 'DJ': 0, 'TWO': 0}
 
+lvltab = [0,46,99,159,229,309,401,507,628,768,928,1112,1324,1567,1847,2168,2537,2961,3448,4008,4651,5389,6237,7212,8332,9618,11095,12792,14742,16982,19555,22510,25905,29805,34285,
+39431,45342,52132,59932,68892,79184,91006,104586,120186,138106,158690,182335,209496,240696,276536,317705,364996,419319,481720,553400,635738,730320,838966,963768,1107128,1271805,
+1460969,1678262,1927866,2214586,2543940,2922269,3356855,3856063,4429503,5088212,5844870,6714042,7712459,8859339,10176758,11690075,13428420,15425254,17719014,20353852,23380486,
+26857176,30850844,35438364,40708040,46761308,53714688,61702024,70877064,81416417,93522954,107429714,123404386,141754466,162833172,187046247,214859767,246809111,283509271,325666684,
+374092835,429719875,493618564,567018884,651333710,748186012,859440093,987237472,1134038112,1302667765,1496372370,1718880532,1974475291,2268076571,2605335878,2992745089,3437761413,
+3948950932,4536153492,5210672106]
+
+lvldef = [46, 53, 60, 70, 80, 92, 106, 121, 140, 160, 184, 212, 243, 280, 321, 369, 424, 487, 560, 643, 738, 848, 975, 1120, 1286, 1477, 1697, 1950, 2240, 2573, 2955, 3395, 3900, 
+4480, 5146, 5911, 6790, 7800, 8960, 10292, 11822, 13580, 15600, 17920, 20584, 23645, 27161, 31200, 35840, 41169, 47291, 54323, 62401, 71680, 82338, 94582, 108646, 124802, 143360, 
+164677, 189164, 217293, 249604, 286720, 329354, 378329, 434586, 499208, 573440, 658709, 756658, 869172, 998417, 1146880, 1317419, 1513317, 1738345, 1996834, 2293760, 2634838, 3026634, 
+3476690, 3993668, 4587520, 5269676, 6053268, 6953380, 7987336, 9175040, 10539353, 12106537, 13906760, 15974672, 18350080, 21078706, 24213075, 27813520, 31949344, 36700160, 42157413, 
+48426151, 55627040, 63898689, 73400320, 84314826, 96852302, 111254081, 127797379, 146800640, 168629653, 193704605, 222508162, 255594759, 293601280, 337259307, 387409211, 445016324, 
+511189519, 587202560]
+
+######################################################################################################################################
+
+def tabfill(xp): 
+    if xp>4536153492:
+        lvl=120
+        a=100
+    else :   
+        lvl=0
+        a=0
+        for l in range(120):
+            if (xp > lvltab[l]):
+                lvl = l+1
+                a = round((((xp- lvltab[l]) / lvldef[l])*100),2)
+    if a == 100:
+        a = 0
+        lvl =+ 1
+    return lvl, a
+
 def DictToList (dictio,listo):
     listo.clear()
     for key, value in dictio.items():
@@ -298,6 +330,31 @@ def LeaderBoard():
         tempo.clear()
             
     return list_lists
+  
+def SearchMembers(guildtag,rnk):
+    members_names = []
+    for skill_name in skill:
+        for k in range(0,200):  
+            url='https://www.curseofaros.com/highscores'
+            headers = {'User-Agent': 'Mozilla/5.0'}        
+            request = Request(url+skill_name+'.json?p='+str(k), headers=headers)
+            html = urlopen(request).read()       
+            data = html.decode("utf-8")        
+            fdata = json.loads(data)
+            for i in range(0,20): 
+                player_name = fdata[i]["name"]
+                tag = player_name.split()[0]
+                tag = tag.upper()
+                
+                if tag == guildtag.upper():
+                    if player_name in members_names :
+                        continue
+                    else:
+                        members_names.append(player_name)
+                        continue
+            if len(members_names) == rnk :
+                break
+        return members_names
 
 ############################################################################
 
@@ -343,7 +400,7 @@ async def bye(ctx):
     await ctx.send(f"See you later {username}!")
 
 @client.command()
-async def bestguild(ctx):
+async def owo(ctx):
     await ctx.send(f"OwO Numba Wan !")
 
 @client.command()
@@ -373,7 +430,7 @@ async def combat(ctx,rank):
     await ctx.send(embed=embedVar1)
     test_list_1.clear()
 
-@client.command(name='mining',aliases=['mine','rocky','pick','kieger'])
+@client.command(name='mining',aliases=['mine','rocky','pick','krieger'])
 async def mining(ctx,rank):
     if ((int(rank)<=0) or (int(rank)>75)):
         await ctx.send("Ranks must be between 1 and 25")
@@ -465,7 +522,7 @@ async def total(ctx,rank):
         test_list_0.clear()
 
 @client.command(name='all',aliases=['overview','ranking'])
-async def all(ctx,rank):
+async def all(ctx):
     mining = get(ctx.guild.emojis, name="mining")
     wc = get(ctx.guild.emojis, name="woodcutting")
     fishing = get(ctx.guild.emojis, name="fishing")
@@ -479,19 +536,17 @@ async def all(ctx,rank):
     field_header = [f' {mining} Top Guilds Mining \n',f' {wc} Top Guilds Woodcutting\n',f' {fishing} Top Guilds Fishing\n',f' {smithing} Top Guilds Smithing\n',
                         f' {crafting} Top Guilds Crafting\n',f' {cooking} Top Guilds Cooking\n',f' {combat} Top Guilds Combat\n',"Top Guilds Total XP\n"]
     
-    if ((int(rank)<=0) or (int(rank)>25)):
-        await ctx.send("Ranks must be between 1 and 25")
-    else:
-        await ctx.send("Fetching Data ... ")
-        embedVar1 = d.Embed(title="Top Guilds", color=0x669999)
-        listed = LeaderBoard()
-        wierd_order = [1,3,5,2,4,6,0,7]
-        for i in range(8) :
-            msg = ""
-            for j in range(int(rank)):
-                msg = msg + rankk(j+1) + ' ' + listed[wierd_order[i]][j]+'\n'
-            embedVar1.add_field(name= field_header[i], value= msg , inline=True)
-        await ctx.send(embed=embedVar1)
+    
+    await ctx.send("Fetching Data ... ")
+    embedVar1 = d.Embed(title="Top Guilds", color=0x669999)
+    listed = LeaderBoard()
+    wierd_order = [1,3,5,2,4,6,0,7]
+    for i in range(8) :
+        msg = ""
+        for j in range(10)):
+            msg = msg + rankk(j+1) + ' ' + listed[wierd_order[i]][j]+'\n'
+        embedVar1.add_field(name= field_header[i], value= msg , inline=True)
+    await ctx.send(embed=embedVar1)
         
 @client.command(name='guildlb',aliases=['guildleaderboard','skillleaderboard'])
 async def guildlb(ctx,skill_name,guildtag,rank):
@@ -510,6 +565,28 @@ async def guildlb(ctx,skill_name,guildtag,rank):
         embedVar8.add_field(name=rankk(i+1), value= test_list_8[i] , inline=False)
     await ctx.send(embed=embedVar8)
     test_list_8.clear()
+    
+    
+@client.command(name="guildcounter",aliases=['gc','counter','howmany','hm'])
+async def guildcounter(ctx,guildtag,rank):
+    guild_name = guildtag.upper()
+    await ctx.send(f"Countings {guild_name}'s members")
+    y = SearchMembers(guild_name,rank)
+    counter_int = len(y)
+    counter_msg = f"{guild_name}'s Members at Top 4000"
+    embedVar8 = d.Embed(title= counter_msg , color=0x0066ff)
+    embedVar8.add_field(name="Count", value= str(counter_int) , inline=False)
+    members_msg = ""
+    for i in range(counter_int):
+        members_msg = y[i] + '\n'
+    if (guild_name == "OWO"):
+        embedVar8.add_field(name="Legends", value= members_msg , inline=False)
+    else:
+        embedVar8.add_field(name="Members", value= members_msg , inline=False)
+    await ctx.send(embed=embedVar8)
+    y.clear()
+    
+
 
 
 @client.command(name='help',aliases=['help?','helpme','commands?','command?','cmd'])
